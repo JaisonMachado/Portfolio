879047191878b0cc42d94b575110f0cc71a002b3
Jaison Machado <jaisonmachado.ans@gmail.com>
2022-11-03 20:37:12 -0300

    modified:   Assets/JavaScript/api.jsmodified:   Assets/JavaScript/main.js 	modified:   Index.html

--- Assets/JavaScript/api.js
+++ Assets/JavaScript/api.js
@@ -1,5 +1,5 @@
 
-async function fatchProfileData(){
+async function fetchProfileData(){
     const url='https://raw.githubusercontent.com/JaisonMachado/Portfolio/main/Assets/Json/profile.json'
     const response = await fetch(url)
     const profileData = await response.json()
